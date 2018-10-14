66197d9a5a8095adab69ad63d64b4d84e810921b
iismail1997 <2015.ismail@gmail.com>
2018-10-14 15:26:33 -0500

    Created test classes we need to create test now

---
+++ test/com/TicTacToe/PlayersTest.groovy
@@ -0,0 +1,34 @@
+package com.TicTacToe
+
+class PlayersTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+    }
+
+    void tearDown() {
+    }
+
+    void testGetPlayer1Name() {
+    }
+
+    void testGetPlayer2Name() {
+    }
+
+    void testGetTurn() {
+    }
+
+    void testGetPly1Score() {
+    }
+
+    void testGetPly2Score() {
+    }
+
+    void testGetTurnCount() {
+    }
+
+    void testTurn() {
+    }
+
+    void testGetCurrentPlayerName() {
+    }
+}
