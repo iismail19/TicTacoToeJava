da189befc07814fcc63e8bed839a4370c1cd570e
Megan OToole <motoole4@luc.edu>
2018-10-16 12:14:29 -0500

    Started on GUI

--- src/com/TicTacToe/Main.java
+++ src/com/TicTacToe/Main.java
@@ -1,7 +1,5 @@
 package com.TicTacToe;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.print("Testing");
-    }
+    public static void main(String[] args){ System.out.print("Testing"); }
 }
